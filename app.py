5d804adef4b0c5e1dac74695876082a8b6346891
ronny7mmobilidade <ronnytj-pva@hotmail.com>
2026-03-23 14:20:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,3 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "BOT 7MOBILIDADE ONLINE 🚀"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "BOT 7MOBILIDADE ONLINE 🚀"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
