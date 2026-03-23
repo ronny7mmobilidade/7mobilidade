66507093b2b7d133797f948a28ead5035d25318a
ronny7mmobilidade <ronnytj-pva@hotmail.com>
2026-03-23 14:38:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "BOT 7MOBILIDADE ONLINE 🚀"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    return "BOT ONLINE 7MOBILIDADE 🚀"
